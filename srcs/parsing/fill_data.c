8f1933d84b6637cb3069050d8ba95942ccec10a0
Kartik Patel <166979981+KartikPat250905@users.noreply.github.com>
2024-12-27 11:05:17 +0200

    changes

--- srcs/parsing/fill_data.c
+++ srcs/parsing/fill_data.c
@@ -1,8 +1,34 @@
 #include "cub3d.h"
 
+//create get path to get the texture and check it
 void	parse_north(t_scene *scene, char *line)
 {
+	int	i;
+	int	start;
 
+	i = 2;
+	while (line[i] && ft_isspace(line[i]))
+		i++;
+	if (i == 2)
+		return ;
+	start = i;
+	while (line[i])
+	{
+		if (!ft_isspace(line[i]))
+			i++;
+		else
+		{
+			printf("Error with path of the north texture");
+			//free and exit
+		}
+	}
+	i = open(line + start, O_RDONLY);
+	if (i < 0)
+		//free and exit
+	close(i);
+	scene->north = ft_strdup(line + start);
+	if (!scene -> north)
+		return ;
 }
 
 void	fill_data(t_scene *scene, t_etype type, char *line)
