055f7c0b67483041a55649460c98b18d8673fe78
Kartik Patel <karpatel@c1r6p15.hive.fi>
2025-01-02 16:40:22 +0200

    Hive work

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -9,6 +9,11 @@
 # include <string.h>
 # include <unistd.h>
 # include <stdio.h>
+# include "MLX42/MLX42.h"
+
+# define WIN_WIDTH 800
+# define WIN_HEIGHT 600
+# define TILE_SIZE 64
 
 typedef enum	e_type
 {
@@ -21,6 +26,21 @@ typedef enum	e_type
 	PARSE_CEILING,
 	PARSE_MAP,
 }		t_etype;
+
+typedef struct	s_player
+{
+	double x;
+	double y;
+	double angle;
+}				t_player;
+
+typedef struct	s_data
+{
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	t_scene		*scene;
+	t_player	player;
+}				t_data;
 
 int		compress_file(t_scene *scene, char *file);
 int		get_line(char **line, int fd);
--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include "./includes/cub3d.h"
 
+void	draw_square(mlx_image_t *image, int x, int y, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < TILE_SIZE)
+	{
+		j = 0;
+		while (j < TILE_SIZE)
+		{
+			if (i == 0 || i == TILE_SIZE - 1 || j == 0 || j == TILE_SIZE - 1)
+				mlx_put_pixel(image, x + i, y + j, 0xFF0000);
+			else
+				mlx_put_pixel(image, x + i, y + j, color);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	render(t_data *data)
+{
+	mlx_delete_image(data->mlx, data->img);
+	data->img = mlx_new_image(data->mlx, WIN_WIDTH, WIN_HEIGHT);
+	mlx_image_to_window(data->mlx, data->img, 0, 0);
+	draw_square(data->img, 1, 1, 0x000000);
+}
+
 int	main(int ac, char **av)
 {
 	t_scene	*scene;
+	t_data	data;
 
 	if (ac != 2)
 		return 1;
@@ -14,7 +44,19 @@ int	main(int ac, char **av)
 	printf("The floor R=%d G=%d and B=%d\n", scene -> floor_color[0], scene->floor_color[1],scene->floor_color[2]);
 	printf("The cieling R=%d G=%d and B=%d\n", scene -> cieling_color[0], scene->cieling_color[1],scene->cieling_color[2]);
 	int i = 0;
+	data.player.x = 1;
+	data.player.y = 1;
+	data.player.angle = 0;
 	while (scene->map[i])
 		printf("%s\n",scene->map[i++]);
+	data.mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "Cub3d", false);
+	if (!data.mlx)
+		return (1);
+	data.img = mlx_new_image(data.mlx, WIN_WIDTH, WIN_HEIGHT);
+	if (!data.img)
+		return (1);
+	mlx_image_to_window(data.mlx, data.img, 0, 0);
+	mlx_loop_hook(data.mlx, (void (*)(void *))render, &data);
+	mlx_loop(data.mlx);
 	free_and_exit(scene, 0);
 }
