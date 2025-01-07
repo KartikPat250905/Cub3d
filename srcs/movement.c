894081c17f43af3802729f357bda540203628d91
Monika <monsulit@gmail.com>
2025-01-07 11:05:41 +0200

    Movement all ways

--- srcs/movement.c
+++ srcs/movement.c
@@ -1,47 +1,58 @@
 #include "cub3d.h"
 
-void print_map(char **map) {
-    for (int i = 0; map[i]; i++) {
-        for (int j = 0; map[i][j]; j++) {
-                printf("%c ", map[i][j]); // Print the map element with a space
-        }
-        printf("\n"); // Newline after each row
-    }
-	printf("\n"); // Newline after each row
+void	print_map(char **map)
+{
+	for (int i = 0; map[i]; i++)
+	{
+		for (int j = 0; map[i][j]; j++)
+			printf("%c ", map[i][j]);
+		printf("\n");
+	}
+	printf("\n");
 }
+
+static void	move_plr(float move_x, float move_y, t_player *p, t_scene *s)
+{
+	if (s->map[(int)p->pos_y][(int)(p->pos_x + move_x)] == '0')
+	{
+		s->map[(int)p->pos_y][(int)p->pos_x] = '0';
+		p->pos_x += move_x ;
+		s->map[(int)p->pos_y][(int)p->pos_x] = 'C';
+	}
+	if (s->map[(int)(p->pos_y + move_y)][(int)p->pos_x] == '0')
+	{
+		s->map[(int)p->pos_y][(int)p->pos_x] = '0';
+		p->pos_y += move_y;
+		s->map[(int)p->pos_y][(int)p->pos_x] = 'C';
+	}
+	print_map(s->map);
+}
+
+
 void	move(t_scene *s, t_player *p, t_dir dir)
 {
-	if (dir == UP)
+	float move_x;
+	float move_y;
+
+	if (dir == UP || dir == DOWN
 	{
-		//printf("Player pos before moving x: %f, y: %f\n", p->pos_x, p->pos_y);
-		//printf("New x in int : %d\n", (int)(p->pos_x + p->dir_x * SPEED));
-		//printf("New pos = %c\n", s->map[(int)(p->pos_x + p->dir_x * SPEED)][(int)p->pos_x]);
-		//if (s->map[(int)(p->pos_x + p->dir_x * SPEED)][(int)p->pos_x] == 0)
-		//{
-		//§	printf("Position edited.\n");
-		//
-			s->map[(int)(p->pos_y)][(int)p->pos_x] = '0';
-			p->pos_x += p->dir_x * SPEED;
-		//}
-		//if (!s->map[(int)(p->pos_y + p->dir_y * SPEED)][(int)p->pos_y])
-		//{
-		//	printf("Position edited.\n");
-			p->pos_y += p->dir_y * SPEED;
-			s->map[(int)p->pos_y][(int)(p->pos_x)] = 'C';
-		//printf("%f + %f * %d = %f\n", p->pos_x, p->dir_x, SPEED, p->pos_x);
-		//printf("Player pos x: %f, y: %f\n", p->pos_x, p->pos_y);
+		move_x = p->dir_x;
+		move_y = p->dir_y;
 	}
-	if (dir == DOWN)
+	else
 	{
-		//printf("Moving down\n");
-		//if (!s->map[(int)(p->pos_x - p->dir_x * SPEED)][(int)p->pos_x])
-			p->pos_x -= p->dir_x * SPEED;
-		//if (!s->map[(int)(p->pos_y - p->dir_y * SPEED)][(int)p->pos_y])
-			p->pos_y -= p->dir_y * SPEED;
-		//printf("Player pos x: %f, y: %f\n", p->pos_x, p->pos_y);
+		move_x = p->pln_x;
+		move_y = p->pln_y;
 	}
-	print_map(s -> map);
-	/*
-	if (dir == LEFT)
-	if (dir == RIGHT)*/
+	if (dir == DOWN || dir == LEFT)
+	{
+		move_x *= -SPEED;
+		move_y *= -SPEED;
+	}
+	else
+	{
+		move_x *= SPEED;
+		move_y *= SPEED;
+	}
+	move_plr(move_x, move_y, p, s);
 }
